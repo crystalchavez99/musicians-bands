9e5623305d411f1e2cf7e0d65ae8c2c8b2cff4fd
crystalchavez99 <chavezcrystal1999@yahoo.com>
2023-11-15 12:44:47 -0800

    created musician and song

--- models/Musician.js
+++ models/Musician.js
@@ -1,7 +1,12 @@
-const {Sequelize, sequelize} = require('../db');
+const {Sequelize, sequelize, DataTypes} = require('../db');
 
 // TODO - define the Musician model
 let Musician;
+
+Musician = sequelize.define('Musician', {
+    name: DataTypes.STRING,
+    instrument: DataTypes.STRING
+})
 
 module.exports = {
     Musician
